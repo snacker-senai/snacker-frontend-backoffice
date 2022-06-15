45b8ca5691fbcdb682173c1a68ff207556ef5c5f
Matheus Santiago <matheus.santiago@premiersoft.net>
2022-06-15 20:24:26 -0300

    Consertando rotas

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import "primereact/resources/primereact.min.css";
 import "primeicons/primeicons.css";
 import "./GlobalStyles.css"
 
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
 
 import { Home } from "./pages/Home";
 import { Employees } from "./pages/Employees";
@@ -13,6 +13,7 @@ import { Orders } from "./pages/Orders";
 import { Deliveries } from "./pages/Deliveries";
 import { PrivateRoute } from "./components/PrivateRoute";
 import { MenuProvider } from "./context/MenuContext";
+import { Login } from "./pages/Login";
 
 export default function App() {
   const ComponentDefault = () => {
@@ -26,16 +27,18 @@ export default function App() {
       <MenuProvider>
         <Router>
           <Switch>
-            <Route path="/home"><PrivateRoute menu={-1} children={<Home />} /></Route>
-            <Route path="/dashboard"><PrivateRoute menu={0} children={<ComponentDefault />} /></Route>
-            <Route path="/employees"><PrivateRoute menu={1} children={<Employees />} /></Route>
-            <Route path="/products"><PrivateRoute menu={2} children={<Products />} /></Route>
-            <Route path="/categories"><PrivateRoute menu={3} children={<Categories />} /></Route>
-            <Route path="/foods"><PrivateRoute menu={4} children={<Orders />} /></Route>
-            <Route path="/deliveries"><PrivateRoute menu={5} children={<Deliveries />} /></Route>
-            <Route path="/perfil"><PrivateRoute menu={6} children={<ComponentDefault />} /></Route>
-            <Route path="/configurations"><PrivateRoute menu={7} children={<ComponentDefault />} /></Route>
-            <Route path="/restaurants"><PrivateRoute menu={8} children={<ComponentDefault />} /></Route>
+            <Route exact path="/home"><PrivateRoute menu={-1} children={<Home />} /></Route>
+            <Route exact path="/dashboard"><PrivateRoute menu={0} children={<ComponentDefault />} /></Route>
+            <Route exact path="/employees"><PrivateRoute menu={1} children={<Employees />} /></Route>
+            <Route exact path="/products"><PrivateRoute menu={2} children={<Products />} /></Route>
+            <Route exact path="/categories"><PrivateRoute menu={3} children={<Categories />} /></Route>
+            <Route exact path="/foods"><PrivateRoute menu={4} children={<Orders />} /></Route>
+            <Route exact path="/deliveries"><PrivateRoute menu={5} children={<Deliveries />} /></Route>
+            <Route exact path="/perfil"><PrivateRoute menu={6} children={<ComponentDefault />} /></Route>
+            <Route exact path="/configurations"><PrivateRoute menu={7} children={<ComponentDefault />} /></Route>
+            <Route exact path="/restaurants"><PrivateRoute menu={8} children={<ComponentDefault />} /></Route>
+            <Route exact path="/login" component={Login} />
+            <Route path="/"><Redirect to="/login" /></Route>
           </Switch>
         </Router>
       </MenuProvider>
