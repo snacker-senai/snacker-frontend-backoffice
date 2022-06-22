84a5702d8b8db03fff7b503774e1b141a6a4dc46
Vitor Rafael <rafaelleite525@gmail.com>
2022-06-22 20:49:44 -0300

    Atualizado a responsividade e alertas das páginas de entregas e pedidos

--- src/pages/Deliveries/index.tsx
+++ src/pages/Deliveries/index.tsx
@@ -1,4 +1,6 @@
-import React, { useEffect, useState } from 'react'
+import { Toast } from 'primereact/toast'
+import React, { useEffect, useRef, useState } from 'react'
+import { Loading } from '../../components/Loading'
 import { OrderCard } from '../../components/OrderCard'
 import { OrderWithProducts } from '../../services/order/Models'
 import { OrderService } from '../../services/order/OrderService'
@@ -7,6 +9,8 @@ import './styles.css'
 
 export const Deliveries = () => {
     const [orders, setOrders] = useState<OrderWithProducts[]>([])
+    const [showSpinnerLoading, setShowSpinnerLoading] = useState<boolean>(false)
+    const toast = useRef<any>(null);
 
     const getOrders = async () => {
         const orders = await OrderService.getOrderByStatusId(2)
@@ -14,28 +18,46 @@ export const Deliveries = () => {
     }
 
     const deliverOrder = async (orderId: number) => {
+        setShowSpinnerLoading(true)
+
         try {
             await OrderService.setOrderStatusByOrderId(orderId, 3)
             const filteredOrders = orders.filter(order => order.id !== orderId)
             setOrders(filteredOrders)
-        } catch (error) {
-            alert(error)
+            showSuccess('Atualizado a entrega com sucesso', '')
+        } catch (error: any) {
+            showError('Erro ao atualizar entrega!', 'Erro: ' + error.message)
         }
+
+        setShowSpinnerLoading(false)
     }
 
     useEffect(() => {
+        setShowSpinnerLoading(true)
         getOrders()
-    
+
         setInterval(() => {
             getOrders()
         }, 5000)
+        setShowSpinnerLoading(false)
     }, [])
+
+    const showSuccess = (sumary, detail: string) => {
+        toast.current.show({ severity: 'success', summary: sumary, detail: detail, life: 3000 });
+    }
+
+    const showError = (sumary, detail: string) => {
+        toast.current.show({ severity: 'error', summary: sumary, detail: detail, life: 3000 });
+    }
 
     return (
         <div className="container-deliveries">
+            <Loading visible={showSpinnerLoading} />
+            <Toast ref={toast} />
             <h1>Listagem de entregas</h1>
             {orders.map(order => (
                 <OrderCard
+                    key={order.id}
                     buttonLabel="Entregar"
                     handleButtonClick={() => deliverOrder(order.id)}
                     table={order.table}
--- src/pages/Orders/index.tsx
+++ src/pages/Orders/index.tsx
@@ -1,4 +1,6 @@
-import React, { useEffect, useState } from 'react'
+import { Toast } from 'primereact/toast'
+import React, { useEffect, useRef, useState } from 'react'
+import { Loading } from '../../components/Loading'
 import { OrderCard } from '../../components/OrderCard'
 import { OrderWithProducts } from '../../services/order/Models'
 import { OrderService } from '../../services/order/OrderService'
@@ -7,6 +9,8 @@ import './styles.css'
 
 export const Orders = () => {
     const [orders, setOrders] = useState<OrderWithProducts[]>([])
+    const [showSpinnerLoading, setShowSpinnerLoading] = useState<boolean>(false)
+    const toast = useRef<any>(null);
 
     const getOrders = async () => {
         const orders = await OrderService.getOrderByStatusId(1)
@@ -14,25 +18,43 @@ export const Orders = () => {
     }
 
     const finishOrder = async (orderId: number) => {
+        setShowSpinnerLoading(true)
+
         try {
             await OrderService.setOrderStatusByOrderId(orderId, 2)
             const filteredOrders = orders.filter(order => order.id !== orderId)
             setOrders(filteredOrders)
-        } catch (error) {
-            alert(error)
+            showSuccess('Atualizado pedido com sucesso', '')
+        } catch (error: any) {
+            showError('Erro ao atualizar pedido!', 'Erro: ' + error.message)
         }
+        setShowSpinnerLoading(false)
     }
 
     useEffect(() => {
+        setShowSpinnerLoading(true)
+
         getOrders()
 
         setInterval(() => {
             getOrders()
         }, 5000)
+
+        setShowSpinnerLoading(false)
     }, [])
+
+    const showSuccess = (sumary, detail: string) => {
+        toast.current.show({ severity: 'success', summary: sumary, detail: detail, life: 3000 });
+    }
+
+    const showError = (sumary, detail: string) => {
+        toast.current.show({ severity: 'error', summary: sumary, detail: detail, life: 3000 });
+    }
 
     return (
         <div className="container-orders">
+            <Loading visible={showSpinnerLoading} />
+            <Toast ref={toast} />
             <h1>Listagem de pedidos</h1>
             {orders.map(order => (
                 <OrderCard
